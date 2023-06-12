7586c7e97f7cfbf8125eb07d26d753866417c6a6
Alobam776 <74178944+Alobam776@users.noreply.github.com>
2023-06-13 00:40:18 +0100

    update disable

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { MdUndo } from "react-icons/md";
 import { GiCheckMark } from "react-icons/gi";
 
 function TaskList(props) {
+  const [disabled, setDisabled] = useState(false);
+
   function handleClick() {
     props.onDelete(props.id);
   }
+
+  const handleToggle = () => {
+    setDisabled(!disabled);
+    props.onToggle();
+  };
+
   return (
-    <div className="note">
+    <div className={`note ${disabled ? "disabled" : ""}`}>
       <h1 style={{ textDecoration: props.completed ? "line-through" : "none" }}>
         {props.title}
       </h1>
       <p style={{ textDecoration: props.completed ? "line-through" : "none" }}>
         {props.content}
       </p>
-      <button onClick={props.onToggle}>
+      <button onClick={handleToggle}>
         {props.completed ? <MdUndo /> : <GiCheckMark />}
       </button>
       <button onClick={handleClick}>
