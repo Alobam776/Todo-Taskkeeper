d9d41cef8a1f22351b12739a7afa5ebe285e4ce6
Alobam776 <74178944+Alobam776@users.noreply.github.com>
2023-06-13 00:48:07 +0100

    update final

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,34 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { MdDelete } from "react-icons/md";
 import { MdUndo } from "react-icons/md";
 import { GiCheckMark } from "react-icons/gi";
 
 function TaskList(props) {
-  const [completed, setCompleted] = useState(props.completed);
-
   function handleClick() {
     props.onDelete(props.id);
   }
-
-  const handleToggle = () => {
-    setCompleted(!completed);
-  };
-
   return (
-    <div className={`note ${completed ? "completed" : ""}`}>
-      <h1 style={{ textDecoration: completed ? "line-through" : "none" }}>
+    <div className="note">
+      <h1 style={{ textDecoration: props.completed ? "line-through" : "none" }}>
         {props.title}
       </h1>
-      <p
-        style={{
-          textDecoration: completed ? "line-through" : "none",
-          pointerEvents: completed ? "none" : "auto",
-        }}
-      >
+      <p style={{ textDecoration: props.completed ? "line-through" : "none" }}>
         {props.content}
       </p>
-      <button onClick={handleToggle}>
-        {completed ? <MdUndo /> : <GiCheckMark />}
+      <button onClick={props.onToggle}>
+        {props.completed ? <MdUndo /> : <GiCheckMark />}
       </button>
       <button onClick={handleClick}>
         <MdDelete />
